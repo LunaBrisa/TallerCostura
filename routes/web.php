24175b45acad50f0ca1b36f2e5f820a9e13c0478
LunaBrisa <danielalunacast0108@gmail.com>
2024-10-28 16:10:20 -0600

    Nose si se va a quedar asi xd

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Pcatalogo', function () {
+    return view('Cliente.PcatalogoView');
+});
+
