b407a47ca8dc56ac18eb57e72fc2ea976752efef
Gera <go47455@gmail.com>
2024-11-14 09:34:18 -0600

    clientes

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -1,13 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Cliente;
+use App\Models\Pedido;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class ClientesController extends Controller
 {
     public function index()
     {
-        return view('clientes.index');
+        $pedidosPorCliente = Pedido::select('cliente_id', DB::raw('count(*) as cantidad_pedidos'))
+            ->groupBy('cliente_id')
+            ->orderBy('cantidad_pedidos', 'desc') 
+            ->limit(3)
+            ->get();
+        $clientes = Cliente::with('persona')->get();
+        
+        return view('clientes.index', compact('clientes', 'pedidosPorCliente'));
     }
 }
