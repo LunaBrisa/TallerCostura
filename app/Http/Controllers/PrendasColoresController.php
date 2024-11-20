58f7adb2802713fa6cc9fd1ad8e5d0bf4066d073
Piturrin22 <esteban.oz220106@gmail.com>
2024-11-20 16:20:10 -0600

    aaaaaaa

--- app/Http/Controllers/PrendasColoresController.php
+++ app/Http/Controllers/PrendasColoresController.php
@@ -28,6 +28,7 @@ class PrendasColoresController extends Controller
         $prendaColor = new PrendaColor();
         $prendaColor -> prenda_id = $saveColorPrendaRequest -> idprenda;
         $prendaColor -> color_id = $saveColorPrendaRequest -> colorprenda;
+        $prendaColor -> ruta_imagen = "pormientras";
         
         $prendaColor -> save();
         
