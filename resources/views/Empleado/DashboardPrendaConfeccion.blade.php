75afff8c037e0e240223cd05abf59310cefc6f09
Piturrin22 <esteban.oz220106@gmail.com>
2024-11-21 12:10:41 -0600

    aaaaa

--- resources/views/Empleado/DashboardPrendaConfeccion.blade.php
+++ resources/views/Empleado/DashboardPrendaConfeccion.blade.php
@@ -332,7 +332,7 @@
 }
 .nav-pills .nav-link:hover {
   background-color: #bc2e96; /* Color al pasar el mouse por encima */
-  color: #F99AAA;
+  color: #F6B2DB;
 }
 </style>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
