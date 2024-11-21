f85bbe66dda807aee0cb8a69c16d619c14c8fae5
Piturrin22 <esteban.oz220106@gmail.com>
2024-11-21 11:06:29 -0600

    empezando a ocultar

--- resources/views/Empleado/DashboardPrendaConfeccion.blade.php
+++ resources/views/Empleado/DashboardPrendaConfeccion.blade.php
@@ -103,6 +103,20 @@
 </div>
 
   <div class="row" style="padding-top: 25px;">
+    <ul class="nav">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
     @foreach ($misPrendas as $prenda)
     <div class="card cardsing mb-4 mx-2" style="width: 18rem;">
       <div class="img-div">
