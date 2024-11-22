4d493abb909f663d9e7692cf51201e8af437c347
LunaBrisa <danielalunacast0108@gmail.com>
2024-11-22 14:12:03 -0600

    se agrego el nav a las demas pantallas

--- resources/views/Empleado/ColoresDePrenda.blade.php
+++ resources/views/Empleado/ColoresDePrenda.blade.php
@@ -10,33 +10,8 @@
 </head>
 <body>
 
-
-<nav class="navbar">
-    <div class="container" >
-        <img src="{{ asset('images/logo.png') }}" width="155" height="85">
-        <a class="navbar-brand" href="#">Taller Costura</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Catalogo</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Servicios</a>
-                </li>
-            </ul>
-            <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Buscar</button>
-            </form>
-        </div>
-    </div>
-</nav><br>
+@extends('layouts.nav')
+@section('content')
 
 <div class="container2">
     <div class="row">
@@ -119,7 +94,7 @@
             @endforeach
     </div>
 </div>
-
+@endsection
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
