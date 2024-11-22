c7eccb0567c242e75230e442662d32d59b566472
Piturrin22 <esteban.oz220106@gmail.com>
2024-11-22 10:56:21 -0600

    subida de gest prendas responsivo

--- resources/views/Empleado/ModificacionPrendaConfeccion.blade.php
+++ resources/views/Empleado/ModificacionPrendaConfeccion.blade.php
@@ -121,7 +121,7 @@
     .btn-modal-sub {
         height: 40px; /* Ajusta el alto del botón en pantallas pequeñas */
         font-size: 14px; /* Tamaño de fuente menor */
-        !important width: 70%;
+        width: 70% !important ;
     }
 }
 
