4573d3c5174833bc9cbc70a022cb2dbb928767d0
Piturrin22 <esteban.oz220106@gmail.com>
2024-11-22 08:21:40 -0600

    responsi jeje

--- resources/views/Empleado/DashboardCatalogo.blade.php
+++ resources/views/Empleado/DashboardCatalogo.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('css/estiloOz.css')}}">
+    <link rel="stylesheet" href="{{asset('css/estoloCatalogoGeneral.css')}}">
     <title>Gestion Catálogo</title>
     <style>
         /* Estilo general */
@@ -99,7 +99,7 @@
                 <div class="cuadradito">
                     <h1 class="Titulo2">Colores</h1>
                     <div class="btn-div">
-                        <button class="btn btn-gest" data-bs-toggle="modal" data-bs-target="#exampleModal"> <p class="p-btn">Agregar Nuevo</p> </button>
+                        <a href="#"><button class="btn btn-gest" data-bs-toggle="modal" data-bs-target="#exampleModal"> <p class="p-btn">Agregar Nuevo</p> </button></a>
                     </div>
 
                     <!-- Modal -->
