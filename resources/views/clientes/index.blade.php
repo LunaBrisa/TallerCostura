0ed6fb3bbd2c5d3ce6fddceb261bbe5433f63fa7
Gera <go47455@gmail.com>
2024-11-21 23:13:49 -0600

    form update clientes

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -66,8 +66,10 @@
                             </div>
                             <div class="modal-body">
                                 <!-- Formulario para editar cliente -->
-                                <form action="/clientes/editar/{{ $cliente->id }}" method="POST">
-                                    @csrf
+                                
+                <form action="{{ route('clientes.update', $cliente->id) }}" method="POST">
+                    @csrf
+                    @method('PUT')
                                     <!-- Campos del formulario -->
                                     <div class="mb-3">
                                         <label for="nombre" class="form-label">Nombre</label>
