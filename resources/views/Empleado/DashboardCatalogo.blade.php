e83ab9138b3f85875966672e856cedc400129857
Piturrin22 <esteban.oz220106@gmail.com>
2024-11-22 08:28:18 -0600

    ahora si responsivo creo

--- resources/views/Empleado/DashboardCatalogo.blade.php
+++ resources/views/Empleado/DashboardCatalogo.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('css/estoloCatalogoGeneral.css')}}">
+    <link rel="stylesheet" href="{{asset('css/estiloCatalogoGeneral.css')}}">
     <title>Gestion Catálogo</title>
     <style>
         /* Estilo general */
@@ -29,8 +29,8 @@
         /* Ajustes específicos para pantallas pequeñas */
         @media (max-width: 576px) {
             .btn-gest {
-                font-size: 0.9rem; /* Reduce el tamaño de la fuente */
-                padding: 8px 15px; /* Reduce el padding */
+                font-size: 0.9rem; 
+                padding: 8px 15px; 
             }
     
             .cuadradito, .cuadradito-solo {
