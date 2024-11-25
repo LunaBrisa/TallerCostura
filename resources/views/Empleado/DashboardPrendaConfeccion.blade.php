bb2758f5dba66cdaaf79234df2c5f92730e6800b
Piturrin22 <esteban.oz220106@gmail.com>
2024-11-25 12:45:10 -0600

    respo

--- resources/views/Empleado/DashboardPrendaConfeccion.blade.php
+++ resources/views/Empleado/DashboardPrendaConfeccion.blade.php
@@ -190,18 +190,18 @@
                   </div>
                   <div class="modal-footer">
                     <div class="row mb-3" style="text-align: center; margin: auto">
-                      <div class="col">
+                      <div class="col mb-3">
                         <a href="/modificar/prenda/{{$prenda -> id}}"><button type="button" class="btn btn-intemodal" style="width: 205px">Gestionar datos de Prenda</button></a>
                       </div>
-                      <div class="col">
+                      <div class="col mb-3">
                         <a href="/modificar/telas-prenda/{{$prenda -> id}}"><button type="button" class="btn btn-intemodal" style="width: 205px">Gestionar Telas</button></a>
                       </div>
                     </div><br>
                     <div class="row" style="margin: auto">
-                      <div class="col">
+                      <div class="col mb-3">
                         <a href="/modificar/colores-prenda/{{$prenda -> id}}"><button type="button" class="btn btn-intemodal" style="width: 205px">Gestionar Colores</button></a>
                       </div>
-                      <div class="col">
+                      <div class="col mb-3">
                         <button type="button" class="btn btn-intemodal" data-bs-dismiss="modal" style="width: 205px">Cerrar</button>
                       </div>
                     </div><br>
@@ -290,18 +290,18 @@
                     </div>
                     <div class="modal-footer">
                       <div class="row mb-3" style="text-align: center; margin: auto">
-                        <div class="col">
+                        <div class="col mb-3">
                           <a href="/modificar/prenda/{{$prenda -> id}}"><button type="button" class="btn btn-intemodal" style="width: 205px">Gestionar datos de Prenda</button></a>
                         </div>
-                        <div class="col">
+                        <div class="col mb-3">
                           <a href="/modificar/telas-prenda/{{$prenda -> id}}"><button type="button" class="btn btn-intemodal" style="width: 205px">Gestionar Telas</button></a>
                         </div>
                       </div><br>
                       <div class="row" style="margin: auto">
-                        <div class="col">
+                        <div class="col mb-3">
                           <a href="/modificar/colores-prenda/{{$prenda -> id}}"><button type="button" class="btn btn-intemodal" style="width: 205px">Gestionar Colores</button></a>
                         </div>
-                        <div class="col">
+                        <div class="col mb-3">
                           <button type="button" class="btn btn-intemodal" data-bs-dismiss="modal" style="width: 205px">Cerrar</button>
                         </div>
                       </div><br>
