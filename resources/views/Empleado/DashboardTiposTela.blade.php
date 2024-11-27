6d2b82736fb9f147bcf389ac25289461b547defa
Piturrin22 <esteban.oz220106@gmail.com>
2024-11-27 11:55:47 -0600

    validaciones y mensajes para tipos de tela

--- resources/views/Empleado/DashboardTiposTela.blade.php
+++ resources/views/Empleado/DashboardTiposTela.blade.php
@@ -29,8 +29,7 @@
                   <div class="modal-header">
                     <h1 class="modal-title fs-5 Titulomodal" id="exampleModalLabel">Agregar Material de Tela</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>s
-                  <!-- BODY -->
+                  </div>
                   <div class="modal-body">
                     <div class="conteform">
                     <form action="/agg/tipotela" method="post">
