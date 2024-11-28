d989162a341e78b4e81af4940fb7609e8f689709
Piturrin22 <esteban.oz220106@gmail.com>
2024-11-28 17:09:15 -0600

    Validando el catalogo

--- resources/views/Empleado/DashboardTelas.blade.php
+++ resources/views/Empleado/DashboardTelas.blade.php
@@ -89,7 +89,7 @@
 
     <div class="row" style="padding-top: 25px">
         @foreach ($misTelas as $tela)
-        <div class="card mb-4 cardsing">
+        <div class="card mb-4 shadow-md cardsing card-hover">
             <div class="card-body">
               <h1 class="list-title">{{$tela->nombre_tela}}</h1>
 
@@ -145,6 +145,12 @@
 </div>
 @endsection
 </body>
+<style>
+    .card-hover:hover {
+        transform: translateY(-10px); /* Desplazamiento hacia arriba */
+        box-shadow: 10px 10px 20px rgba(0, 0.2, 0.2, 0.2); /* Sombra más intensa */
+    }
+</style>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
 
