e814dc321c4a712ae4e5162be239b09ec94ba18c
IsaiasSilva01 <24170017@uttcampus.edu.mx>
2024-11-28 20:45:32 -0600

    Arreglando vista detalles pedido x5

--- resources/views/Cliente/detalles.blade.php
+++ resources/views/Cliente/detalles.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detalles del Pedido</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/estiloCatalogoGeneral.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/detalles.css') }}">
+
 </head>
 <body>
     <div class="container mt-5">
