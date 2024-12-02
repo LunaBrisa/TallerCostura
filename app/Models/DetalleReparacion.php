aa16f953e59f7abd8997667687bf5ee5ec4adaf5
Vane-BV <24170012@uttcampus.edu.mx>
2024-12-02 17:50:56 -0600

    arreglo de ver mis pedidos

--- app/Models/DetalleReparacion.php
+++ app/Models/DetalleReparacion.php
@@ -22,6 +22,11 @@ class DetalleReparacion extends Model
     {
     return $this->belongsToMany(Servicio::class, 'REPARACIONES_SERVICIOS', 'detalle_reparacion_id', 'servicio_id');
     }
+// En App\Models\DetalleReparacion
+public function insumos()
+{
+    return $this->belongsToMany(Insumo::class, 'detalle_reparacion_insumo', 'detalle_reparacion_id', 'insumo_id');
+}
 
 
     
