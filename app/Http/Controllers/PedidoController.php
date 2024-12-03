1ca6857df90f910807b3d526438c577581667459
Gera <go47455@gmail.com>
2024-12-02 21:43:53 -0600

    a

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -116,7 +116,7 @@ public function store(Request $request)
         'detalles_reparaciones.*.prenda' => 'required|string|max:100',
         'detalles_reparaciones.*.cantidad' => 'required|integer|min:1',
         'detalles_reparaciones.*.descripcion_problema' => 'nullable|string|max:500',
-        'detalles_reparaciones.*.servicio' => 'required|exists:servicios,id',
+        'detalles_reparaciones.*.servicio' => 'required|exists:SERVICIOS,id',
         'detalles_reparaciones.*.precio_prenda' => 'nullable|numeric|min:0',
     ]);
 
