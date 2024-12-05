463ffb737ff8622505d0be44bf00fe1a1f2b8d3f
IsaiasSilva01 <24170017@uttcampus.edu.mx>
2024-12-04 23:41:12 -0600

    Resuelto conflicto en AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,20 +2,15 @@
 
 namespace App\Providers;
 
+
 use Illuminate\Support\ServiceProvider;
-<<<<<<< HEAD
 use Illuminate\Pagination\Paginator;
-=======
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
- 
->>>>>>> 51c56f78b371845e08e503a1345fe618db601af4
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+  
     public function register(): void
     {
         //
@@ -23,17 +18,17 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-<<<<<<< HEAD
-       Paginator::useBootstrap(); 
-=======
-        //
+        // Configurar la paginación 
+        Paginator::useBootstrap();
+    
+        // Personalizar el email de verificación
         VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
             return (new MailMessage)
                 ->subject('Verify Email Address"111')
                 ->line('Click the button below to verify your email address.')
                 ->action('Verify Email Address', $url);
         });
->>>>>>> 51c56f78b371845e08e503a1345fe618db601af4
     }
+}    
 
-}
+
