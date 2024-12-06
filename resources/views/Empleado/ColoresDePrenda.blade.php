82efae651f62edfff4ba0be39de4bdbdcf070346
IsaiasSilva01 <24170017@uttcampus.edu.mx>
2024-12-06 14:51:51 -0600

    Responsividad en cambios PRendacolor

--- resources/views/Empleado/ColoresDePrenda.blade.php
+++ resources/views/Empleado/ColoresDePrenda.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> 
-    <link rel="stylesheet" href="{{asset('css/estiloOz.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/estiloOz.css') }}">
     <title>Colores de Prenda</title>
 </head>
 <body>
@@ -13,232 +13,104 @@
 @extends('layouts.nav')
 @section('content')
 
-<div class="container2" style="height: 100vh;">
-    <div class="row">
-      <div class="col">
-        <h1 class="Titulo1">Colores de la Prenda</h1>
-      </div>
-      <div class="col" style="padding-top: 50px;">
-        <div class="btn-div">
-          <!-- Button trigger modal -->
-          <button type="button" class="btn btn-agregprenda" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Agregar Nuevo Color
-          </button>
+<div class="container" style="height: 100vh; padding-top: 20px;">
+    <div class="row mb-4 text-center">
+        <div class="col">
+            <h1 class="Titulo1">Colores de la Prenda</h1>
+        </div>
+    </div>
 
-          <!-- Modal -->
-          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h1 class="modal-title fs-5 Titulomodal" id="exampleModalLabel">Agregar Color a la Prenda</h1>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                  <form action="/agreg/color-prenda" method="post" enctype="multipart/form-data">
-                    @csrf
-                    <input type="hidden" name="idprenda" value="{{$miPrenda->id}}">
-                    
-                    <label for="colorprenda"><h3 class="h3-modal">Color de la Prenda</h3></label>
-                    <select name="colorprenda" class="form-select" aria-label="Default select example">
-                      <option selected>Seleccionar el Color de la Prenda</option>
-                      @foreach ($misColores as $color)
-                        <option value="{{$color->id}}">"{{$color->color}}"</option>
-                      @endforeach
-                    </select><br>
+    <div class="row mb-4">
+        <div class="col text-center">
+            <button type="button" class="btn btn-agregprenda" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Agregar Nuevo Color
+            </button>
+        </div>
+    </div>
 
-                    <label for="imagencolorsote"><h3 class="h3-modal">Imagen de la Prenda con el Color</h3></label>
-                    <input class="form-control" type="file" name="imagencolorsote" accept="image/*" required><br>
-
-                    <div class="btn-div">
-                      <input type="submit" class="btn btn-modal-sub" value="Guardar">
-                    </div>
-                  </form>
-                </div>
-              </div>
+    <div class="row mb-4">
+        <div class="col">
+            @foreach ($errors->all() as $error)
+            <div class="alert alert-danger text-center">
+                {{ $error }}
             </div>
-          </div>
+            @endforeach
         </div>
-      </div>  
     </div>
 
     <div class="row">
-      <div class="col">
-        @foreach ($errors->all() as $error)
-        <div class="alert alert-danger">
-            {{ $error }}
-        </div>
-      @endforeach
-      </div>
-    </div>
-
-    <div class="row" style="padding-top: 25px">
-      <div class="col">
         @foreach ($misPrendasColores as $prendaColor)
-                    <div class="card mb-4 shadow-md card-hover cardsing" style="width: 100%; margin: auto;">
-                        <div class="card-body">
-                          <div class="img-div" style="width: 45%; heihgt: 40%; margin: auto;">
-                            <img src="{{ asset($prendaColor -> ruta_imagen) }}" class="card-img-top" alt="...">
-                          </div><br>
-                            <h1 class="list-title">{{$prendaColor->color}}</h1> 
-                            <div class="color-circle" style="background-color: {{ $prendaColor->color }}; margin: auto; text-align: center;" title="{{ $prendaColor->color }}" readonly></div><br>
-                        <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-modal-sub" data-bs-toggle="modal" data-bs-target="#exampleModalElim{{$prendaColor->id}}">
-                          Eliminar
-                        </button><br>
-                        <button type="button" class="btn btn-modal-sub" data-bs-toggle="modal" data-bs-target="#exampleModalImg{{$prendaColor->id}}">
-                          Cambiar Imagen
-                        </button><br>
-                        
-                      </div>
+        <div class="col-md-4 mb-4">
+            <div class="card shadow-md card-hover cardsing mx-auto" style="max-width: 300px;">
+                <div class="img-div">
+                    <img src="{{ asset($prendaColor->ruta_imagen) }}" class="card-img-top" alt="Imagen del color">
+                </div>
+                <div class="card-body text-center">
+                    <h5 class="list-title">{{ $prendaColor->color }}</h5>
+                    <div class="color-circle mx-auto" style="background-color: {{ $prendaColor->color }};"></div>
+                    <div class="mt-3">
+                        <button type="button" class="btn btn-modal-sub" data-bs-toggle="modal" data-bs-target="#exampleModalElim{{ $prendaColor->id }}">
+                            Eliminar
+                        </button>
+                        <button type="button" class="btn btn-modal-sub mt-2" data-bs-toggle="modal" data-bs-target="#exampleModalImg{{ $prendaColor->id }}">
+                            Cambiar Imagen
+                        </button>
                     </div>
-            @endforeach
-      </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
     </div>
 </div>
+
 @endsection
 
 @foreach ($misPrendasColores as $prendaColor)
-  <!-- Modal -->
-  <div class="modal fade" id="exampleModalElim{{$prendaColor->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- Modal para eliminar -->
+<div class="modal fade" id="exampleModalElim{{ $prendaColor->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5 Titulomodal" id="exampleModalLabel">Eliminacion de Tela</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <img src="{{ asset('images/warning-sign-icon-transparent-background-free-png.webp') }} " class="img-cent">
-          <p class="advert">Advertencia! ¿Estás seguro de que quieres eliminar este color de la prenda?</p>
-
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-intemodal" data-bs-dismiss="modal">Cancelar</button>
-          <a href="/elim/color/prenda/{{$prendaColor->id}}" class="btn btn-intemodal">Eliminar</a>
-        </div>
-      </div>
-    </div>
-  </div>
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModalImg{{$prendaColor->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
         <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5 Titulomodal" id="exampleModalLabel">Eliminacion de Tela</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <form action="/modif/img-color-prenda" method="post" enctype="multipart/form-data"> 
-              @csrf
-              <input type="hidden" name="idprenda" value="{{$prendaColor->id}}">
-
-              <label for="imagencolorsillo"><h3 class="h3-modal">Imagen Nueva de la Prenda con el Color</h3></label><br><br>
-              <input class="form-control" type="file" name="imagencolorsillo"><br> 
-              <button type="button" class="btn btn-modal-sub" data-bs-dismiss="modal">Cancelar</button><br>
-              <input type="submit" class="btn btn-modal-sub" value="Guardar">  
-            </form>
-          </div>
-          <div class="modal-footer">
-          </div>
+            <div class="modal-header">
+                <h5 class="modal-title Titulomodal">Eliminar Color</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+                <img src="{{ asset('images/warning-sign-icon-transparent-background-free-png.webp') }}" class="img-cent" alt="Advertencia">
+                <p class="advert">¿Estás seguro de eliminar este color?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-intemodal" data-bs-dismiss="modal">Cancelar</button>
+                <a href="/elim/color/prenda/{{ $prendaColor->id }}" class="btn btn-intemodal">Eliminar</a>
+            </div>
         </div>
-      </div>
     </div>
+</div>
+
+<!-- Modal para cambiar imagen -->
+<div class="modal fade" id="exampleModalImg{{ $prendaColor->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title Titulomodal">Cambiar Imagen</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="/modif/img-color-prenda" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <input type="hidden" name="idprenda" value="{{ $prendaColor->id }}">
+                    <label for="imagencolorsillo" class="form-label h3-modal">Nueva Imagen</label>
+                    <input type="file" class="form-control" name="imagencolorsillo" accept="image/*" required>
+                    <div class="mt-3 text-center">
+                        <button type="button" class="btn btn-intemodal" data-bs-dismiss="modal">Cancelar</button>
+                        <input type="submit" class="btn btn-modal-sub" value="Guardar">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 @endforeach
 
-</body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
 </html>
-<style>
-
-.color-circle {
-    width: 20px; /* Ancho del círculo */
-    height: 20px; /* Alto del círculo */
-    border-radius: 50%; /* Hace el elemento circular */
-    border: 1px solid #ccc; /* Opcional: borde para visibilidad */
-    display: inline-block; /* Asegura que el círculo sea un bloque en línea */
-}
-
-
-.img-cent { /* Centra la imagen */
-        margin: auto !important;
-        text-align: center !important;
-        display: block;
-
-    }
-    .navbar {
-         background-color: black;
-     }
- 
-     .navbar a {
-         color: white;
-     }
- 
-     .navbar a:hover {
-         color: lightgray;
-     }
- 
-     .navbar-toggler-icon {
-         filter: invert(1);
-     }
-
-     .card {
-        transition: transform 0.3s ease, box-shadow 0.3s ease;
-    }
-
-    .card-hover:hover {
-        transform: translateY(-10px); /* Desplazamiento hacia arriba */
-        box-shadow: 10px 10px 20px rgba(0, 0.2, 0.2, 0.2); /* Sombra más intensa */
-    }
-
-     .btn-modal-sub {
-    margin: 10px auto; 
-    display: block; 
-    width: 100%; 
-    max-width: 280px; 
-    height: 45px; 
-    background-color: #BE5A8C;
-    border: 2px solid #F99AAA;
-    color: #FFCDD4;
-    font-size: 16px;
-    text-align: center;
-    padding: 0;
-}
-
-.btn-modal-sub:hover {
-    background-color: #F99AAA;
-    color: #BE5A8C;
-}
-
-.cardsing {
-    width: 100%; 
-    max-width: 320px;
-    margin: 10px auto; 
-    padding: 10px;
-}
-
-
-@media (max-width: 576px) {
-    .btn-modal-sub {
-        font-size: 14px; /* Reduce el tamaño del texto en pantallas pequeñas */
-        height: 40px; /* Ajusta la altura del botón */
-        max-width: 100%; /* Se expande al 100% del contenedor */
-    }
-
-    .cardsing {
-        padding: 5px; /* Espaciado más pequeño */
-    }
-}
-
-.h3-modal {
-    margin: auto;
-    text-align: center;
-}
-
-@media (max-width: 576px) {
-    .card{
-        width: 70vw;
-    }
-}
-
- </style>
- 
